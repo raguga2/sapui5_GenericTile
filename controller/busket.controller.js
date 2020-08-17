6d1e2ece8e2c3b2bc806e8f5f1f969b9270b9dbd
raguga2 <33901698+raguga2@users.noreply.github.com>
2020-08-17 21:44:17 +0300

    .

--- controller/busket.controller.js
+++ controller/busket.controller.js
@@ -10,34 +10,56 @@ sap.ui.define([
 	ResourceModel) {
   //"use strict";
 
-  return Controller.extend('namespace.controller.busket', {
+  return Controller.extend('namespace.controller.busket',{ 
 
+format1:function(Quantity,Cost){
+return Quantity*Cost;
+},
+      
+      
+format2:function(aBusketData){
+    debugger;
+if (aBusketData!=null){
+var sum=0;
+    for(var i=0;i<aBusketData.length;i++){
+    sum+=aBusketData[i].Cost* aBusketData[i].Quantity;  
+    //    sum+=aBusketData[i].format1()+sum;
+    };
+return sum;    
+}
+return 0;
+},
+
+      
      onAdd: function(oEvent) {
 	let sBusketRowPath = oEvent.getSource().getBindingContext("main").getPath();// путь массива event.getsource() 
 	let oModel = oEvent.getSource().getModel('main')
 	let oBusketItem = oModel.getProperty(sBusketRowPath)// строка
 	let Quantity = oBusketItem.Quantity
 	let namenotebook = oBusketItem.namenotebook
+    let Cost =oBusketItem.Cost
 	let oCatalog = oModel.getProperty("/catalog") // все товары каталога
 	//console.dir(sBusketRowPath);
 	let aFindEl = oCatalog.find(oBusketItem => oBusketItem.namenotebook == namenotebook)
 	let QuanityStock = aFindEl.Quantity
 	if (QuanityStock > Quantity) {
-		Quantity++
+    		Quantity++ 
 	}
-	oModel.setProperty(sBusketRowPath + "/Quantity", Quantity)
+ 	oModel.setProperty(sBusketRowPath + "/Quantity", Quantity)
 	},
-		
+
 	onDelete: function(oEvent) {
 		let sBusketRowPath = oEvent.getSource().getBindingContext("main").getPath();
 		let oModel = oEvent.getSource().getModel('main')
 		let oBusketItem = oModel.getProperty(sBusketRowPath)
 		let Quantity = oBusketItem.Quantity
+        let Cost = oBusketItem.Cost
 		let aBusketData = oModel.getProperty("/busket") // массив все данных элемента
 		//console.dir(sBusketRowPath);
 		Quantity--
 		if (oBusketItem.Quantity > 0) {
 			oModel.setProperty(sBusketRowPath + "/Quantity", Quantity)
+            
 		}
 			let aFilteredBusket = aBusketData.filter(oBusketItem => oBusketItem.Quantity > 0);
 			oModel.setProperty("/busket", aFilteredBusket)
